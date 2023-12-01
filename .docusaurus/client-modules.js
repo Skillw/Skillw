bc53b791ab2d5e26eaa5d60373dddcf8943410d1
Glom-c <glom@skillw.com>
2023-12-02 00:29:41 +0800

    Del Summary

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/glom/桌面/git/Website/node_modules/infima/dist/css/default/default.css"),
+  require("/home/glom/桌面/git/Website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/glom/桌面/git/Website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/glom/桌面/git/Website/src/css/custom.css"),
+];
